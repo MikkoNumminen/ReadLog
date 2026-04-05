7c9f1d84fe9c6117323d0c738595cbe4aaf0671c
mikko numminen <mikko@mikkos-MacBook-Pro.local>
2026-04-05 14:00:08 +0300

    fix: remove custom signin page reference

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,4 @@ import { prisma } from "@/lib/db";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
-  pages: {
-    signIn: "/signin",
-  },
 });
